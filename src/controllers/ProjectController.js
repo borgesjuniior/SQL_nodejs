8a9c471c47dc0a8631aa786ebd192c671275d955
Valdecy Júnior <valdecyborgesjr@hotmail.com>
2020-12-02 12:59:01 -0300

    Criado o relacionamento das tabelas

--- src/controllers/ProjectController.js
+++ src/controllers/ProjectController.js
@@ -3,10 +3,18 @@ const knex = require('../database/connection');
 module.exports = {
     async index(req, res, next) {
 
-        try {
+        try { 
+            const { user_id } = req.query; //Pega a query da url.
 
-            const results = await knex('projects');
-            return res.json(results);
+            const query = knex('projects'); //recebe os projetos.
+
+            if (user_id) {  //Verfica se o user_id existe e faz a connsulta.
+                query.where({user_id})
+            }
+
+            const results = await query; //recebe a consulta e coloca na variável.
+            
+            return res.json(results) //Retorna o json com os resultados.
             
         } catch (error) {
             next(error);   
